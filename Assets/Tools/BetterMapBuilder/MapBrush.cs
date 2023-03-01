297624bb53d147f4d7b7808dcb479eb403b6546e
ShadierPond <basm.jewishy120@gmail.com>
2023-03-01 23:12:16 +0100

    worked on weapon in blender
    - created new asset BetterMapBuilder WIP

---
+++ Assets/Tools/BetterMapBuilder/MapBrush.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+namespace Tools.BetterMapBuilder
+{
+    public class MapBrush
+    {
+        public Texture2D Texture;
+        public GameObject GameObject;
+        public int RotationY;
+        
+        public MapBrush(Texture2D texture, GameObject gameObject, int rotationY)
+        {
+            Texture = texture;
+            GameObject = gameObject;
+            RotationY = rotationY;
+        }
+        
+        public MapBrush(Texture2D texture, GameObject gameObject) : this(texture, gameObject, 0)
+        {
+        }
+        
+        public MapBrush(Texture2D texture) : this(texture, null, 0)
+        {
+        }
+        
+        public MapBrush(GameObject gameObject) : this(null, gameObject, 0)
+        {
+        }
+        
+        public MapBrush() : this(null, null, 0)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, int rotationY) : this(texture, null, rotationY)
+        {
+        }
+        
+        public MapBrush(GameObject gameObject, int rotationY) : this(null, gameObject, rotationY)
+        {
+        }
+        
+        public MapBrush(int rotationY) : this(null, null, rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, GameObject gameObject, float rotationY) : this(texture, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, float rotationY) : this(texture, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(GameObject gameObject, float rotationY) : this(null, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(float rotationY) : this(null, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, GameObject gameObject, double rotationY) : this(texture, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, double rotationY) : this(texture, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(GameObject gameObject, double rotationY) : this(null, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(double rotationY) : this(null, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, GameObject gameObject, long rotationY) : this(texture, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, long rotationY) : this(texture, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(GameObject gameObject, long rotationY) : this(null, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(long rotationY) : this(null, null, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, GameObject gameObject, short rotationY) : this(texture, gameObject, (int) rotationY)
+        {
+        }
+        
+        public MapBrush(Texture2D texture, short rotationY) : this(texture, null, (int) rotationY)
+        {
+        }
+    }
+}
---
+++ Assets/Tools/BetterMapBuilder/MapCell.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Tools.BetterMapBuilder
+{
+    [System.Serializable]
+    public class MapCell
+    {
+        public Vector2Int Position;
+        public GameObject GameObject;
+        public Texture2D Texture;
+        public int RotationY;
+
+        public MapCell(Vector2Int position, GameObject gameObject, Texture2D texture, int rotationY)
+        {
+            Position = position;
+            GameObject = gameObject;
+            Texture = texture;
+            RotationY = rotationY;
+        }
+    }
+}
